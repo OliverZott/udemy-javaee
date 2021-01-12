4b825842a888dbe0248eb91c1ebf282fe7506cb8
Oliver Zott <zott_oliver@web.de>
2021-01-12 18:14:35 +0100

    Adding validations for POST request

--- hello-todo/src/main/java/org/project/entity/Todo.java
+++ hello-todo/src/main/java/org/project/entity/Todo.java
@@ -1,11 +1,16 @@
 package org.project.entity;
 
 import java.time.LocalDate;
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Todo {
@@ -14,8 +19,15 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty(message = "Task must be set.")
+    @Size(min = 10, message = "Task hast to be at least 10 characters.")
     private String task;
+
+    @NotNull(message = "Date must be set.")
+    @FutureOrPresent(message = "Date must be in present or future.")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
     private boolean isCompleted;
